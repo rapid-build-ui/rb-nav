89619d226f371343272d94062d4d1598155eebc2
Judd Younce <jyounce@yahoo.com>
2017-12-14 19:08:10 -0600

    feat(active): complete default, false and hash options

---
+++ src/client/scripts/activity.js
@@ -0,0 +1,113 @@
+/******************
+ * ACTIVITY MODULE
+ ******************/
+const ACTIVE_CLASS = 'active';
+const Activity = superClass => class extends superClass {
+	/*************
+	 * PUBLIC API
+	 *************/
+	// Properties
+ 	static get properties() {
+ 		return {
+ 			active: {
+				type: String, // :boolean | string | object
+ 				value: true
+ 			},
+			activity: {
+				type: String,
+				readOnly: true,
+				computed: 'getActivity(active)'
+			},
+			activeClass: {
+				type: String,
+				readOnly: true,
+ 				value: ACTIVE_CLASS
+ 			}
+ 		}
+ 	}
+
+	getActivity(active) { // :string (activity type)
+		switch(active) {
+			case 'false': active = false;  break;
+			case 'hash':  active = 'hash'; break;
+			case 'path':  active = 'path'; break;
+			default:      active = 'default';
+		}
+		return active;
+	}
+
+	// Event Handling
+	detachActivityEvents() { // :this
+		if (!this.activity) return this;
+		this.removeEventListener('click', this._setActive);
+		if (this.activity === 'hash')
+			window.removeEventListener('hashchange', this._setActiveHash);
+		return this;
+	}
+
+	attachActivityEvents() { // :this
+		if (!this.activity) return this;
+		this.addEventListener('click', this._setActive);
+		return this;
+	}
+
+	// Methods
+	setActivity() { // :this
+		if (!this.activity) return this;
+		this._slot.addEventListener('slotchange', e => {
+			switch(this.activity) {
+				case 'hash':
+					this._setActiveHash(e);
+					this._setActiveHash = this._setActiveHash.bind(this);
+					window.addEventListener('hashchange', this._setActiveHash);
+					break;
+			}
+		})
+		return this;
+	}
+
+	/**********
+	 * PRIVATE
+	 **********/
+	// Helpers
+	_activateLink(link) { // :void
+		link.classList.add(ACTIVE_CLASS);
+	}
+	_deactivateLink(link) { // :void
+		if (!this._isActiveLink(link)) return;
+		link.classList.remove(ACTIVE_CLASS);
+	}
+	_deactivateLinks() { // :void
+		for (let link of this.links) {
+			this._deactivateLink(link);
+		}
+	}
+	_isActiveLink(link) { // :boolean
+		return link.classList.contains(ACTIVE_CLASS);
+	}
+
+	// Event Handlers
+	_setActiveHash(e) { // :void
+		var hash = location.hash;
+		if (!hash) return;
+		for (let link of this.links) {
+			let href = link.getAttribute('href');
+			if (!href || !href.includes(hash)) continue;
+			this._deactivateLinks();
+			this._activateLink(link);
+			break;
+		}
+	}
+
+	_setActive(e) { // :void
+		if (!this.links.length) return this;
+		let link = e.composedPath()[0];
+		if (link.tagName.toLowerCase() !== 'a') return;
+		if (this._isActiveLink(link)) return;
+		this._deactivateLinks();
+		this._activateLink(link);
+	}
+
+}
+
+export default Activity;
--- src/client/scripts/rb-nav.js
+++ src/client/scripts/rb-nav.js
@@ -4,23 +4,25 @@
 import { Element as PolymerElement } from '../../../@polymer/polymer/polymer-element.js';
 // import { DomRepeat as DomRepeat } from '../../../@polymer/polymer/lib/elements/dom-repeat.js';
 // import { DomBind as DomBind } from '../../../@polymer/polymer/lib/elements/dom-bind.js';
+import Activity from './activity.js';
 import template from '../views/rb-nav.html';
 
-export class RbNav extends PolymerElement {
-	/* Lifecycle
-	 ************/
+export class RbNav extends Activity(PolymerElement) {
 	constructor() {
 		super();
 		this.importPath = '/node_modules/@rapid-build-ui/rb-nav';
 	}
-	ready() {
-		super.ready();
+	connectedCallback() {
+		super.connectedCallback();
 		this._slot = this.root.querySelector('slot');
 		this.setTabIndexes();
-		this.attachEvents();
+		this.setActivity()
+		this.attachActivityEvents();
+		// console.log(this.activity);
 	}
 	disconnectedCallback() {
-		this.removeEventListener('click', this.setActive);
+		super.disconnectedCallback();
+		this.detachActivityEvents()
 	}
 
 	/* Properties
@@ -87,7 +89,7 @@ export class RbNav extends PolymerElement {
 
 	/* Getters
 	 **********/
-	get _links() { // :element[]
+	get links() { // :element[]
 		return this._slot
 			.assignedNodes({flatten:true})
 			.filter(n => n.nodeType === Node.ELEMENT_NODE)
@@ -97,33 +99,11 @@ export class RbNav extends PolymerElement {
 	/* Private
 	 **********/
 	setTabIndexes() { // :this
-		if (!this._links.length) return this;
-		for (let link of this._links) {
+		if (!this.links.length) return this;
+		for (let link of this.links) {
 			if (link.hasAttribute('tabindex')) continue;
 			link.setAttribute('tabindex', 0);
 		}
-		return this;
-	}
-
-	/* Event Handlers
-	 *****************/
-	setActive(e) { // :void
-		if (!this._links.length) return this;
-		let link = e.composedPath()[0];
-		if (link.tagName.toLowerCase() !== 'a') return;
-		if (link.classList.contains('active')) return;
-		for (let link of this._links)
-			if (link.classList.contains('active')) {
-				link.classList.remove('active');
-				break;
-			}
-		link.classList.add('active');
-	}
-
-	/* Attach Events
-	 ****************/
-	attachEvents() { // :this
-		this.addEventListener('click', this.setActive);
 		return this;
 	}
 
